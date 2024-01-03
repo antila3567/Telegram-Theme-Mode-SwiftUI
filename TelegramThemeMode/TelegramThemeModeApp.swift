4cfd811e4674ede42493df4a077435a2a3827b50
IvanL <antila3567@gmail.com>
2024-01-03 14:54:03 +0200

    Initial Commit

---
+++ TelegramThemeMode/TelegramThemeModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelegramThemeModeApp.swift
+//  TelegramThemeMode
+//
+//  Created by Иван Легенький on 03.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TelegramThemeModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
